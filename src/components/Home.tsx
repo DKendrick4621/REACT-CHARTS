069c0087b9822e0cab62a0a50204a17d66c1a886
DEREK KENDRICK <derekkednrick79@gmail.com>
2022-05-04 23:50:50 -0400

    RadialChart

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,26 @@
 import * as React from "react";
 import DeviceChart from "./DeviceChart";
 import Layout from "./Layout";
-
+import { name } from "../utils/generators";
+import Radialchart from "./Radialchart";
 export default function Home() {
   return (
     <Layout>
-      <DeviceChart />
+      <h2>{name("Jordan", "Hudgens")}</h2>
+      <DeviceChart
+        options={{
+          colors: ["#32a852", "#d94016", "#450c34"],
+          labels: ["Desktop", "Mobile", "Tablet"],
+        }}
+      />
+
+      <DeviceChart
+        options={{
+          colors: ["#030002", "#03adfc", "#6bfc03"],
+          labels: ["Smartphones", "laptops", "ipads"],
+        }}
+        />
+      <Radialchart />
     </Layout>
   );
 }
